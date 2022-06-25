53e09d631dea47ca34747da3448c14000e3f3d6b
Zeld2 <theudelci@gmail.com>
2022-06-25 17:32:48 -0300

    Fixed exer4

--- exer4/CalculoDesconto.cs
+++ exer4/CalculoDesconto.cs
@@ -4,17 +4,17 @@ namespace exer4
     {
         public static void TabelaDesconto(double valorCompra)
         {
-            double percentual = 0;
+            int percentual = 0;
             var valorFinal = valorCompra;
             if(valorCompra<300)
             {
-                System.Console.WriteLine($"R${valorCompra} - {percentual*100}% - R${valorFinal}");
+                System.Console.WriteLine($"R${valorCompra} - {percentual}% - R${valorFinal}");
             }
             else
             {
-                percentual = 0.01+(Math.Floor((valorCompra-300)/100))/100;
-                valorFinal = valorCompra * (1-percentual);
-                System.Console.WriteLine($"R${valorCompra} - {percentual*100}% - R${valorFinal}");
+                percentual = 1+(((int)valorCompra)-300)/100;
+                valorFinal = valorCompra * (1-((double)percentual/100));
+                System.Console.WriteLine($"R${valorCompra} - {percentual}% - R${valorFinal}");
 
             }
         }
